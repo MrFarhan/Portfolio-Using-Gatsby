b9e983483898c4dcd2c0c2268c877c3c15351097
MrFarhan <muhammadfarhan012345@gmail.com>
2021-07-16 22:41:40 +0500

    Created Portfolio Using Gatsby

---
+++ src/components/GithubButtons/GithubButtons.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import GitHubButton from 'react-github-btn';
+
+const GithubButton = () => (
+  <>
+    <GitHubButton
+      className="github-button"
+      href="https://github.com/MrFarhan"
+      data-icon="octicon-repo-forked"
+      data-size="large"
+      data-show-count="true"
+    >
+      Fork
+    </GitHubButton>
+    <GitHubButton
+      className="github-button"
+      href="https://github.com/MrFarhan"
+      data-icon="octicon-star"
+      data-size="large"
+      data-show-count="true"
+    >
+      Star
+    </GitHubButton>
+  </>
+);
+
+export default GithubButton;
